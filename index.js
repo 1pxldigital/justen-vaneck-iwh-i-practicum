d17de8e92de28a2077d42150571c910710b28b6e
Justen van Eck <justen.vaneck@outlook.com>
2026-09-23 09:17:38 -0400

    create vessel records via POST /update-cobj added and tested

--- index.js
+++ index.js
@@ -37,11 +37,24 @@ app.get('/update-cobj', (req, res) => {
 res.render('updates', {title: 'Update Custom Object Form | Integrating with HubSpot I Practicum'});
 });
 
-// * Code for Route 2 goes here
-
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newRecord = {
+        properties: {
+            name: req.body.name,
+            length_ft: req.body.length_ft,
+            mooring: req.body.mooring
+        }
+    };
+    try {
+        await axios.post(BASE, newRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+    console.error(error.response ? error.response.data : error.message);
+    res.status(500).send('Oops! Error creating new record in HubSpot');
+}
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
